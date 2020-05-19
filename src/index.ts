65413132a7e6e84f8db934430c0851001dcbeb20
polatengin <polatengin@outlook.com>
2020-05-19 09:35:16 +0300

    company and symbol list to track

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
 Notification.requestPermission();
+
+const companies = [
+  { name: 'Microsoft', symbol: 'MSFT', isAdded: false, data: undefined },
+  { name: 'Apple', symbol: 'AAPL', isAdded: false, data: undefined },
+  { name: 'Amazon', symbol: 'AMZN', isAdded: false, data: undefined },
+  { name: 'Facebook', symbol: 'FB', isAdded: false, data: undefined },
+  { name: 'Google', symbol: 'GOOGL', isAdded: false, data: undefined },
+  { name: 'IBM', symbol: 'IBM', isAdded: false, data: undefined },
+  { name: 'Intel', symbol: 'INTC', isAdded: false, data: undefined },
+  { name: 'Oracle', symbol: 'ORCL', isAdded: false, data: undefined },
+  { name: 'Tesla', symbol: 'TSLA', isAdded: false, data: undefined }
+];
