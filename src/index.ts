bd7d6e74ea2f5ca583408f967d966caa4146435c
polatengin <polatengin@outlook.com>
2020-05-19 09:36:22 +0300

    loop through companies and create an element for each of them

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,15 @@ const companies = [
   { name: 'Oracle', symbol: 'ORCL', isAdded: false, data: undefined },
   { name: 'Tesla', symbol: 'TSLA', isAdded: false, data: undefined }
 ];
+
+companies.forEach(item => {
+  const element = document.createElement('button');
+  element.className = 'list-group-item list-group-item-action';
+  element.innerText = `(${item.symbol}) ${item.name}`;
+
+  element.addEventListener('click', () => {
+    element.classList.toggle('active');
+    element.blur();
+
+});
+  });
